da14a825bd6d764137f8ab1b8c6f6cc9a6573b8f
enrico <enrico200165@tiscali.it>
2012-10-25 16:40:44 +0200

    prefisso 0 ai folders con nr < 10

--- learn_php_mysql_etc_PHP/04/example4-1.php
+++ learn_php_mysql_etc_PHP/04/example4-1.php
@@ -1,4 +1,7 @@
 <?php
+
+// mostra che in PHP the constant FALSE is defined as NULL, 
+// or nothing.
 echo "a: [" . (20 > 9) . "]<br />";
 echo "b: [" . (5 == 6) . "]<br />";
 echo "c: [" . (1 == 0) . "]<br />";
