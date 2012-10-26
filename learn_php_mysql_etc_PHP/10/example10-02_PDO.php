05bb3c30fcaf7dd58eeeda06cb0e53bfc28a32eb
enrico200165@tiscali.it <enrico200165@tiscali.it>
2012-10-26 09:34:49 +0200

    "added" un paio di files dimenticati

---
+++ learn_php_mysql_etc_PHP/10/example10-02_PDO.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'login.php';
+
+
+try {
+	$dbh = new PDO('mysql:host=localhost;dbname=kissakoit', $db_username,$db_password);
+	
+	$stmt = $dbh->query('SELECT name, value from variable');
+	
+	$row_count = $stmt->rowCount();
+	print "there are nr rows: ${row_count}\n";
+	
+	foreach($stmt as $row) {
+//		print($row['name']) . "<br />";
+		print($row['name']);
+		print_r("> ".$row['value']);		
+		//		(()) . "> " . $row['value'] . "\n";
+		print("\n");		
+		
+	}
+	$dbh = null;
+} catch (PDOException $e) {
+	print "Error!: " . $e->getMessage() . "<br/>";
+	die();
+}
+
+
+
+?>
