630ac9b4fa09d79fa0af3a1838e5495082edc1a8
enrico200165@tiscali.it <enrico200165@tiscali.it>
2012-10-30 07:32:32 +0100

    agiungi esempi mancanti cap 10

--- learn_php_mysql_etc_PHP/10/example10-02_PDO.php
+++ learn_php_mysql_etc_PHP/10/example10-02_PDO.php
@@ -1,22 +1,31 @@
 <?php
 require_once 'login.php';
 
+$sapi = php_sapi_name() ;
+echo $sapi;
+
+function evNL() {
+	switch ( php_sapi_name()) {
+		case "cli": return "\n";
+		break;
+		default: return "<br />";
+	}
+}
 
 try {
-	$dbh = new PDO('mysql:host=localhost;dbname=kissakoit', $db_username,$db_password);
-	
+	$dbh = new PDO("mysql:host=localhost;dbname=${db_database}", $db_username,$db_password);
+
 	$stmt = $dbh->query('SELECT name, value from variable');
-	
+
 	$row_count = $stmt->rowCount();
 	print "there are nr rows: ${row_count}\n";
-	
+
 	foreach($stmt as $row) {
-//		print($row['name']) . "<br />";
+		//		print($row['name']) . "<br />";
 		print($row['name']);
-		print_r("> ".$row['value']);		
-		//		(()) . "> " . $row['value'] . "\n";
-		print("\n");		
-		
+		print_r("> ".$row['value']);
+		print(evNL());
+
 	}
 	$dbh = null;
 } catch (PDOException $e) {
@@ -24,6 +33,6 @@ try {
 	die();
 }
 
-
+echo $sapi;
 
 ?>
